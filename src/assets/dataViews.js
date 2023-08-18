47178da9d8af0c42e7825f8f4cf12adb30f99cf3
matmolni <matmolni@gmail.com>
2023-08-18 21:50:41 +0200

    started work on data panel implementation

--- src/assets/dataViews.js
+++ src/assets/dataViews.js
@@ -51,35 +51,35 @@ const dataViewLayouts = [
             [
                 {
                     id: 'item-1', x: 0, y: 0, w: 2, h: 1,
-                    dataSource: "lap"
+                    chartType: "lap"
                 },
                 {
                     id: 'item-2', x: 2, y: 0, w: 2, h: 1,
-                    dataSource: "gpsSpeed"
+                    chartType: "gpsSpeed"
                 },
                 {
                     id: 'item-3', x: 4, y: 0, w: 2, h: 1,
-                    dataSource: "gpsSpeed"
+                    chartType: "gpsSpeed"
                 },
                 {
                     id: 'item-4', x: 0, y: 1, w: 2, h: 2,
-                    dataSource: "gpsSpeed"
+                    chartType: "gpsSpeed"
                 },
                 {
                     id: 'item-5', x: 2, y: 1, w: 2, h: 2,
-                    dataSource: "gpsSpeed"
+                    chartType: "gpsSpeed"
                 },
                 {
                     id: 'item-6', x: 4, y: 1, w: 1, h: 1,
-                    dataSource: "gpsSpeed"
+                    chartType: "gpsSpeed"
                 },
                 {
                     id: 'item-7', x: 5, y: 1, w: 1, h: 1,
-                    dataSource: "gpsSpeed"
+                    chartType: "gpsSpeed"
                 },
                 {
                     id: 'item-8', x: 4, y: 2, w: 2, h: 1,
-                    dataSource: "gpsSpeed"
+                    chartType: "gpsSpeed"
                 }
             ]
     },
---
+++ src/components/DataPanel.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import GPSSpeedPanel from "./data-panels/GPSSpeedPanel";
+
+function DataPanel({chartType}) {
+
+    let ChartComponent = null;
+    let panelName = null;
+
+    switch (chartType) {
+        case "lap":
+            ChartComponent = () => <div>Lap</div>;
+            panelName = "Lap Chart";
+            break;
+        case "gpsSpeed":
+            ChartComponent = () => <div>GPS Speed</div>;
+            panelName = "GPS Speed Chart";
+            break;
+        case "pie":
+            ChartComponent = () => <div>Pie Chart</div>;
+            panelName = "Pie Chart";
+            break;
+        case "scatter":
+            ChartComponent = () => <div>Scatter Chart</div>;
+            panelName = "Scatter Chart";
+            break;
+        default:
+            ChartComponent = () => <div>Default Chart</div>;
+            panelName = "Default Chart";
+    }
+
+
+    return (
+        <div className="flex-grow-1 m-1 p-3 border-round-lg bg-bluegray-800">
+            <div className="text-lg text-white font-bold">{panelName}</div>
+            <ChartComponent></ChartComponent>
+        </div>
+    );
+}
+
+export default DataPanel;
